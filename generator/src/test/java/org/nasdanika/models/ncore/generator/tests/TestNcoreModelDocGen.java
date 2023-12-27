27298a89b1fbf274f6de6c73205fd6d3087454b8
Pavel <Pavel.Vlasov@nasdanika.org>
2023-12-27 18:26:33 -0500

    2023.12.1 - upgrade to action generator

--- generator/src/test/java/org/nasdanika/models/ncore/generator/tests/TestNcoreModelDocGen.java
+++ generator/src/test/java/org/nasdanika/models/ncore/generator/tests/TestNcoreModelDocGen.java
@@ -3,7 +3,6 @@ package org.nasdanika.models.ncore.generator.tests;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -13,38 +12,20 @@ import java.util.function.Function;
 
 import org.eclipse.emf.common.util.DiagnosticException;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.junit.jupiter.api.Test;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.Diagnostic;
 import org.nasdanika.common.DiagramGenerator;
 import org.nasdanika.common.ExecutionException;
 import org.nasdanika.common.MutableContext;
-import org.nasdanika.common.NasdanikaException;
 import org.nasdanika.common.NullProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
-import org.nasdanika.common.Transformer;
 import org.nasdanika.diagramgenerator.plantuml.PlantUMLDiagramGenerator;
-import org.nasdanika.graph.Connection;
-import org.nasdanika.graph.Element;
-import org.nasdanika.graph.emf.EObjectNode;
-import org.nasdanika.graph.processor.NopEndpointProcessorConfigFactory;
-import org.nasdanika.graph.processor.ProcessorConfig;
-import org.nasdanika.graph.processor.ProcessorInfo;
-import org.nasdanika.graph.processor.emf.EObjectNodeProcessorReflectiveFactory;
 import org.nasdanika.html.model.app.Action;
-import org.nasdanika.html.model.app.Label;
-import org.nasdanika.html.model.app.Link;
 import org.nasdanika.html.model.app.gen.ActionSiteGenerator;
-import org.nasdanika.html.model.app.graph.WidgetFactory;
-import org.nasdanika.html.model.app.graph.emf.EObjectReflectiveProcessorFactoryProvider;
-import org.nasdanika.models.ecore.graph.EcoreGraphFactory;
+import org.nasdanika.models.ecore.graph.processors.EcoreActionGenerator;
 import org.nasdanika.models.ecore.graph.processors.EcoreNodeProcessorFactory;
 import org.nasdanika.models.ncore.processors.ecore.EcoreGenNcoreProcessorsFactory;
 import org.nasdanika.ncore.NcorePackage;
@@ -58,23 +39,7 @@ public class TestNcoreModelDocGen {
 	
 	@Test
 	public void testGenerateNcoreModelDoc() throws IOException, DiagnosticException {
-		List<EPackage> ePackages = Arrays.asList(EcorePackage.eINSTANCE, NcorePackage.eINSTANCE);
 		ProgressMonitor progressMonitor = new NullProgressMonitor(); // new PrintStreamProgressMonitor();
-		Transformer<EObject,Element> graphFactory = new Transformer<>(new EcoreGraphFactory());
-		Map<EObject, Element> graph = graphFactory.transform(ePackages, false, progressMonitor);
-
-		NopEndpointProcessorConfigFactory<WidgetFactory> configFactory = new NopEndpointProcessorConfigFactory<>() {
-			
-			@Override
-			protected boolean isPassThrough(Connection connection) {
-				return false;
-			}
-			
-		};
-		
-		Transformer<Element,ProcessorConfig> processorConfigTransformer = new Transformer<>(configFactory);				
-		Map<Element, ProcessorConfig> configs = processorConfigTransformer.transform(graph.values(), false, progressMonitor);
-		
 		MutableContext context = Context.EMPTY_CONTEXT.fork();
 		context.register(DiagramGenerator.class, new PlantUMLDiagramGenerator());
 		Consumer<Diagnostic> diagnosticConsumer = d -> d.dump(System.out, 0);
@@ -94,73 +59,20 @@ public class TestNcoreModelDocGen {
 				diagnosticConsumer,
 				ecoreGenNcoreProcessorFactory);
 		
-		EObjectNodeProcessorReflectiveFactory<WidgetFactory, WidgetFactory> eObjectNodeProcessorReflectiveFactory = new EObjectNodeProcessorReflectiveFactory<>(ecoreNodeProcessorFactory);
-		EObjectReflectiveProcessorFactoryProvider eObjectReflectiveProcessorFactoryProvider = new EObjectReflectiveProcessorFactoryProvider(eObjectNodeProcessorReflectiveFactory);
-		Map<Element, ProcessorInfo<Object>> registry = eObjectReflectiveProcessorFactoryProvider.getFactory().createProcessors(configs.values(), false, progressMonitor);
-		
-		WidgetFactory ncoreProcessor = null;
-		Collection<Throwable> resolveFailures = new ArrayList<>();		
-		URI baseActionURI = URI.createURI("local://ncore.models.nasdanika.org/");
-		
 		Map<EPackage, URI> packageURIMap = Map.ofEntries(
-			Map.entry(EcorePackage.eINSTANCE, URI.createURI("https://ecore.models.nasdanika.org/")),			
-			Map.entry(NcorePackage.eINSTANCE, baseActionURI)	
+			Map.entry(EcorePackage.eINSTANCE, URI.createURI("https://ecore.models.nasdanika.org/"))			
 		);
 		
-		for (EPackage topLevelPackage: ePackages) {
-			for (Entry<Element, ProcessorInfo<Object>> re: registry.entrySet()) {
-				Element element = re.getKey();
-				if (element instanceof EObjectNode) {
-					EObjectNode eObjNode = (EObjectNode) element;
-					EObject target = eObjNode.get();
-					if (target == topLevelPackage) {
-						ProcessorInfo<Object> info = re.getValue();
-						Object processor = info.getProcessor();
-						if (processor instanceof WidgetFactory) {
-							WidgetFactory widgetFactoryNodeProcessor = (WidgetFactory) processor;
-							widgetFactoryNodeProcessor.resolve(packageURIMap.get(topLevelPackage), progressMonitor);
-							
-							if (topLevelPackage == NcorePackage.eINSTANCE) { 							
-								ncoreProcessor = widgetFactoryNodeProcessor;
-							}
-						}
-					}
-				}
-			}			
-		}
-		
-		if (!resolveFailures.isEmpty()) {
-			NasdanikaException ne = new NasdanikaException("Theres's been " + resolveFailures.size() +  " failures during URI resolution: " + resolveFailures);
-			for (Throwable failure: resolveFailures) {
-				ne.addSuppressed(failure);
-			}
-			throw ne;
-		}								
-		
-		ResourceSet actionModelsResourceSet = new ResourceSetImpl();
-		actionModelsResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
-		
 		File actionModelsDir = new File("target\\action-models\\");
-		actionModelsDir.mkdirs();
-		
+		actionModelsDir.mkdirs();		
 		File output = new File(actionModelsDir, "ncore.xmi");
-		Resource actionModelResource = actionModelsResourceSet.createResource(URI.createFileURI(output.getAbsolutePath()));
-		Collection<Label> labels = ncoreProcessor.createLabelsSupplier().call(progressMonitor, diagnosticConsumer);
-		for (Label label: labels) {
-			if (label instanceof Link) {
-				Link link = (Link) label;
-				String location = link.getLocation();
-				if (!org.nasdanika.common.Util.isBlank(location)) {
-					URI uri = URI.createURI(location);
-					if (!uri.isRelative()) {
-						link.setLocation("${base-uri}" + uri.deresolve(baseActionURI, true, true, true).toString());
-					}
-				}
-			}
-		}
-						
-		actionModelResource.getContents().addAll(labels);
-		actionModelResource.save(null);
+		
+		EcoreActionGenerator eCoreActionGenerator = new EcoreActionGenerator(
+				NcorePackage.eINSTANCE, 
+				packageURIMap, 
+				ecoreNodeProcessorFactory);
+		
+		eCoreActionGenerator.generateActionModel(diagnosticConsumer, output, progressMonitor);
 				
 		String rootActionResource = "actions.yml";
 		URI rootActionURI = URI.createFileURI(new File(rootActionResource).getAbsolutePath());//.appendFragment("/");
